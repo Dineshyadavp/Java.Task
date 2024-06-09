26a9f542b8b4f2d17515a8ad75ee680e2994f994
Dinesh Yadav P <143863984+Dineshyadavp@users.noreply.github.com>
2024-06-09 18:56:26 +0530

    Create NaturalNumbers.java

---
+++ NaturalNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class NaturalNumbers {
+    
+  public static void main(String[] args)
+    {     
+    int i;
+	System.out.println ("The first 10 natural numbers are:\n");
+	for (i=1;i<=36;i++)
+	{      
+		System.out.print (i + " ");
+	}
+ System.out.println ("\n");
+}
+}
