04300572765d62e587a0c7177e636c32bfa20d12
Dinesh Yadav P <143863984+Dineshyadavp@users.noreply.github.com>
2024-06-09 19:02:25 +0530

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Substring {
+    public static void main(String[] args) {
+        String str = "Dinesh, DYP!";
+        String substr1 = str.substring(6);
+        System.out.println("Substring 1: " + substr1); 
+        String substr2 = str.substring(6,11);
+        System.out.println("Substring 2: " + substr2);
+    }
+}
