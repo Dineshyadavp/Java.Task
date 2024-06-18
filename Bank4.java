976dcb730a753b007a7ab2cafee9e39ece8542f4
Dinesh Yadav P <143863984+Dineshyadavp@users.noreply.github.com>
2024-06-19 00:03:32 +0530

    Create Bank4.java

---
+++ Bank4.java
@@ -0,0 +1,44 @@
+public class Bank4 {
+    
+        static int currentBalance = 1000;
+        public static void greetCustomer ()
+        {
+            System.out.println("Hello,Welcome to the Banking Application");
+        }
+        public static int accountNumber = 36261646;
+         public static void accountNumber ()
+    {
+        System.out.println("Account Number Ready");
+    }
+    public static int accountPassword =  123456;
+    public static void accountPassword ()
+    {
+        System.out.println("Account Password Ready");
+    }
+        public static void deposit(int amount) {
+            currentBalance = currentBalance+amount;
+            System.out.println("Amount is deposited Successfully");
+        }
+        public static void withdraw(int amount){
+            currentBalance = currentBalance - amount;
+            System.out.println("Amount is Whithdrawn Successfully");
+        }
+        public int getCurrentBalance()
+        {
+            return currentBalance;
+        }
+        public static void main(String[] args) {
+                greetCustomer();
+                System.out.println("Current Balance is :" ); //+bank1.getCurrentBalance());
+                accountNumber();
+                System.out.println("Current Balance is :");
+                accountPassword();
+                System.out.println("Current Balance is :");
+                deposit(500);
+                System.out.println("Current Balance is :"); // +bank1.getCurrentBalance());
+                withdraw(300);
+                System.out.println("Current Balance is :");  //+bank1.getCurrentBalance());
+    
+        }
+    
+        }
